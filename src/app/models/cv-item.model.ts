a4444fa224170ff37ce13d5a0cfd3169ac988637
Toby Harding <rael@hardingconsulting.com>
2020-04-10 07:53:12 +0100

    Updated CV component

---
+++ src/app/models/cv-item.model.ts
@@ -0,0 +1,12 @@
+export class CvItem {
+  title: string;
+  subtitle: string;
+  begin?: string;
+  end?: string;
+  description?: string;
+  achievements?: string[];
+  tags?: string[];
+  link?: string;
+  thumbnail?: string;
+  attachment?: string;
+}
